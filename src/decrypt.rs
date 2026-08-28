4ed7fdf808d0c5e38a9132fa0683597ee65d15a4
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2026-08-28 06:59:52 -0400

    Comment updates in decrypt

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -40,6 +40,20 @@ impl From<(i32, i32, TryFromIntError)> for DecryptError {
 /// This is more or less a direct translation from the game's own "Decypher_Test.cs"
 /// file (found on "DEC Solutions Mainframe" under "Staff/J.Scott", saved as "Decypher_Test.dec")
 pub fn decrypt(data: &str, passcode: u16) -> Result<String, DecryptError> {
+    /*
+    The code snippit in-game operates on values of the C# `int` type, which is a 32 bit signed integer.
+    It has to be 32-bit (rather than 8 or 16) because the encoding process makes the number larger
+    (through additions and a multiply).
+
+    I'm not sure it has to be *signed*. In theory, an encrypted output value should only ever be positive,
+    since it's calculated by taking the (presumably positive) ordinal value of a character,
+    adding a positive constant and an unsigned passcode, and multiplying by a positive
+    constant. Making it unsigned eliminates a possible error case for us, since we need to convert
+    from i32 to u32 before we can convert to char.
+
+    In practice, I'd rather mimic the original behavior more closely than eliminate an error case
+    that's unlikely to actually come up and a conversion that's fairly cheap, so we use i32 rather than u32.
+    */
     const HALFMAX: i32 = (u16::MAX / 2) as i32;
     let passcode = passcode as i32;
 
@@ -52,8 +66,8 @@ pub fn decrypt(data: &str, passcode: u16) -> Result<String, DecryptError> {
         out.push(as_char);
     }
 
-    //This is maybe inefficient - we take a vec of char, create a new String
-    //(which might involve an allocation), then use trim (which gives a &str) and
-    //use to_owned to turn that into a String (which might involve another allocation)
+    //This feels inefficient since we're building a string, getting
+    //a slice from it via trim, then immediately building a new string,
+    //but I think it's the only real way to do this
     Ok(out.into_iter().collect::<String>().trim().to_owned())
 }
