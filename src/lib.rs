339e33a5024ba91b51eb77500f8e5ea953f76640
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2026-08-27 20:50:04 -0400

    Actually decrypt both headers and file body at once

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
-pub mod decrypt;
+use thiserror::Error;
 
+use crate::decrypt::decrypt;
+
+pub mod decrypt;
 pub mod headers;
+
+#[derive(Error, Debug)]
+/// Reasons the process of decrypting a file can fail
+pub enum DecypherError {
+    #[error("Missing either headers or body.")]
+    MissingHeadersOrBody,
+
+    #[error("Failed to decrypt headers of provided file")]
+    DecryptHeaders(#[from] headers::HeaderDecryptError),
+
+    #[error("Failed to decrypt body of provided file")]
+    DecryptBody(#[from] decrypt::DecryptError)
+}
+
+#[derive(Debug)]
+/// A decrypted file along with its header values
+pub struct Decyphered {
+    pub header_values: headers::HeaderValues,
+    pub body: String
+}
+
+pub fn decypher_str(file_content: &str) -> Result<Decyphered, DecypherError> {
+    let mut split = file_content.split('\n');
+    let encrypted_headers = split.next().ok_or(DecypherError::MissingHeadersOrBody)?;
+    let encrypted_body = split.next().ok_or(DecypherError::MissingHeadersOrBody)?;
+
+    let header_values = headers::decrypt_headers(encrypted_headers)?;
+    let body = decrypt(encrypted_body, header_values.passcode)?;
+    Ok(Decyphered {
+        header_values,
+        body
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 use clap::Parser;
-use hn_decypher::{
-    decrypt::DecryptError,
-    headers::{HeaderDecryptError, decrypt_headers}
-};
-use std::fs;
-use std::io;
+use hn_decypher::{DecypherError, decypher_str};
 use std::path::PathBuf;
+use std::{fs, io};
 use thiserror::Error;
 
 fn main() -> Result<(), Error> {
     let args = Args::parse();
 
-    let encrypted = fs::read_to_string(args.dec_path).map_err(Error::FileRead)?;
-    let encrypted_headers = encrypted.split('\n').next().unwrap();
-    let decrypted_headers = decrypt_headers(encrypted_headers)?;
-    println!("{:?}", decrypted_headers);
+    let encrypted = fs::read_to_string(args.in_path).map_err(Error::FileRead)?;
+    let decyphered = decypher_str(&encrypted)?;
+    println!("{:?}", decyphered.header_values);
+    fs::write(args.out_path, decyphered.body).map_err(Error::FileWrite)?;
     Ok(())
 }
 
@@ -26,11 +22,8 @@ enum Error {
     #[error("Failed to write output file")]
     FileWrite(#[source] io::Error),
 
-    #[error("Failed to decrypt headers of provided file")]
-    DecryptHeaders(#[from] HeaderDecryptError),
-
-    #[error("Failed to decrypt body of provided file")]
-    DecryptBody(#[from] DecryptError)
+    #[error("Failed to decypher file")]
+    DecypherError(#[from] DecypherError)
 }
 
 /// Decrypts the \"dec\" file format from the video game Hacknet
@@ -38,6 +31,10 @@ enum Error {
 #[command(version, about)]
 struct Args {
     /// Path to the .dec file you want to decrypt
-    #[arg(value_name = "FILE_PATH")]
-    dec_path: PathBuf
+    #[arg(value_name = "INPUT_FILE_PATH")]
+    in_path: PathBuf,
+
+    /// Path that the decrypted file should be written to
+    #[arg(value_name = "OUTPUT_FILEPATH", default_value = "./out.txt")]
+    out_path: PathBuf
 }
