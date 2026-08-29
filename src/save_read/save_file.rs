178fd63eaedc25a38f62eed919f065cc535ffbb9
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2026-08-29 19:47:27 -0400

    Functionality for reading a particular file from a particular computer

--- src/save_read/save_file.rs
+++ src/save_read/save_file.rs
@@ -44,8 +44,24 @@ impl<'a, 'input, 'b> ComputerFindStrategy<'a, 'input, 'b> {
     }
 }
 
+//some convinience functions for identifying noteworthy types of element
+fn is_element_of_type<'a, 'input>(node: &Node<'a, 'input>, tagname: &str) -> bool {
+    //not sure if we need to check if it's an element since I beleive it can't have
+    //a tag name if it's not an element, but I'd rather be certain it is an element than save
+    //whatever (presumably miniscule) amount of time it takes to double check
+    node.is_element() && node.has_tag_name(tagname)
+}
 fn is_computer<'a, 'input>(node: &Node<'a, 'input>) -> bool {
-    node.is_element() && node.tag_name().name() == "computer"
+    is_element_of_type(node, "computer")
+}
+fn is_filesystem<'a, 'input>(node: &Node<'a, 'input>) -> bool {
+    is_element_of_type(node, "filesystem")
+}
+fn is_folder<'a, 'input>(node: &Node<'a, 'input>) -> bool {
+    is_element_of_type(node, "folder")
+}
+fn is_file<'a, 'input>(node: &Node<'a, 'input>) -> bool {
+    is_element_of_type(node, "file")
 }
 
 /// A `computer` node in an XML save file. Represents one in-game computer.
@@ -97,5 +113,66 @@ impl<'a, 'input, 'b> Computer<'a, 'input> {
     /// Take the underlying [Node] backing this Computer
     pub fn into_inner(self) -> Node<'a, 'input> {
         self.node
+    }
+
+    /// Get a reference to the root "folder" element of this Computer's filesystem element
+    fn rootdir(&self) -> Option<Node<'a, 'input>> {
+        //step 1: get the filesystem element
+        let filesystem = self.node.children().find(is_filesystem)?;
+
+        //step 2: get the root folder element; an element with tag name "folder" and "name" attribute "/"
+        filesystem
+            .children()
+            .find(|n| is_folder(n) && n.attribute("name").is_some_and(|a| a == "/"))
+    }
+
+    /// Get a particular file on this Computer
+    ///
+    /// Returns the file element [Node] if a file matching the given `path` can be found
+    /// on this Computer, or None if no file matching the given `path` can be found
+    ///
+    /// This function's treatment of paths may not exactly match treatment of paths
+    /// by the actual game, or your operating system's treatment of paths:
+    ///
+    /// - The path is always treates as absolute - this can be thought of as
+    ///   though a "/" is prepended to the path before looking for the file.
+    /// - Forward slashes "/" and backslashes "\\" are treated identically
+    /// - References to the current directory (".") and parent directory ("..")
+    ///   are ignored entirely
+    /// - Contiguous seperators (e.g. "//") are treated the same as a single seperator ("/")
+    /// - Items are case-sensitive ("This_File_Name" is different from "this_file_name")
+    //TODO: perhaps eliminate case-sensitivity? Autocompletion in-game isn't case sensitive; don't know if file path references themselves are or not.
+    pub fn file(&self, path: &str) -> Option<Node<'a, 'input>> {
+        let mut split = path.split(&['/', '\\']);
+
+        // getting the filename first means that our searching through the filesystem structure
+        // only needs to pay attention to folders, meaning our strategy for looping through the folders
+        // still works if one of the folders in our path has the same name as a file (which I believe can legitimately happen)
+        let filename = split.next_back()?;
+
+        // iterate through the path parts, finding a folder with a matching name for each part.
+        let mut current_folder = self.rootdir()?;
+        for pathpart in split {
+            if pathpart.is_empty() || pathpart == "." || pathpart == ".." {
+                continue;
+            }
+
+            current_folder = current_folder
+                .children()
+                .find(|f| is_folder(f) && f.attribute("name").is_some_and(|n| n == pathpart))?;
+        }
+        // now, current_folder is the folder that should contain our file
+        // simply look for a file by the specified name in current_folder and return whatever we find
+        current_folder
+            .children()
+            .find(|f| is_file(f) && f.attribute("name").is_some_and(|n| n == filename))
+    }
+
+    /// Get the content of a particular file on this Computer
+    ///
+    /// Convinience method for `Computer.file(path).and_then(|f| f.text())`; see [Computer::file] for
+    /// more details.
+    pub fn file_content(&self, path: &str) -> Option<&str> {
+        self.file(path).and_then(|f| f.text())
     }
 }
